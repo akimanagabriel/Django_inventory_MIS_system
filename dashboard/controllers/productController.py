e78c1a2731aa15912c090fc32477724d0a34af55
akimanagabriel <akimanagabriel@gmail.com>
2023-04-13 20:33:43 +0000

    crud of product page

--- dashboard/controllers/productController.py
+++ dashboard/controllers/productController.py
@@ -1,8 +1,10 @@
-from django.shortcuts import redirect, render
+from django.shortcuts import redirect, render, get_object_or_404
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from main.models import Category, Incoming
 from django.contrib import messages
+from datetime import datetime
+from main.validation import *
 
 
 @login_required
@@ -19,9 +21,36 @@ def create(request):
 def store(request):
     productName = request.POST.get('productTitle')
     categoryId = request.POST.get('categoryId')
-    productPrice = request.POST.get('price')
-    productQuantity = request.POST.get('quantity')
+    productPrice = int(request.POST.get('price'))
+    productQuantity = int(request.POST.get('quantity'))
     expirationDate = request.POST.get('expiratioDate')
+
+    # validate dates
+    expiration = datetime.strptime(expirationDate, '%Y-%m-%d').date()
+    now = datetime.now().date()
+    days_left = expiration-now
+
+    if days_left.days < 1:
+        messages.error(request, "Denied, Your product already expired!")
+        return redirect('/dashboard/stock/')
+
+    if productPrice < 5:
+        messages.error(request, "product price must start atleast 5RWF")
+        return redirect('/dashboard/stock/')
+
+    if productQuantity < 1:
+        messages.error(request, "quantity can not be less than 1")
+        return redirect('/dashboard/stock/')
+
+    if len(productName) < 3:
+        messages.error(
+            request, "Product title must be atleast 3 characters long")
+        return redirect('/dashboard/stock/')
+
+    existing = Incoming.objects.filter(name=productName)
+    if existing:
+        messages.error(request, "Product already exist!")
+        return redirect("/dashboard/stock/")
 
     product = Incoming(
         price=productPrice,
@@ -31,13 +60,19 @@ def store(request):
         category=Category.objects.filter(id=categoryId)[0]
     )
     product.save()
-    messages.success(request,"Product created")
+    messages.success(request, "Product created")
     return redirect('/dashboard/stock/')
 
 
 @login_required
 def show(request, id):
-    return HttpResponse('product show')
+    product = get_object_or_404(Incoming, id=id)
+    return render(request,'product/show.html',{
+        "product": product,
+        "products":Incoming.objects.filter(category=product.category),
+        # "category": get_object_or_404(Category,id=product.category.id)
+        }
+    )
 
 
 @login_required
@@ -52,4 +87,6 @@ def update(request, id):
 
 @login_required
 def destroy(request, id):
-    return HttpResponse('product destroy')
+    Incoming.objects.filter(id=id).delete()
+    messages.success(request, "Product removed successfully!")
+    return redirect('/dashboard/stock/')
