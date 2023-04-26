436b19388903f70a14db7997895d089712f38e8e
akimanagabriel <akimanagabriel@gmail.com>
2023-04-26 17:43:32 +0000

    refactor settings.py

--- base/settings.py
+++ base/settings.py
@@ -11,9 +11,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%fxg(tga*w3(dn4kj#p1=f7v$%qj0&2yu=(=vxa7ghuu*@25nw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
